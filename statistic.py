6c483809b859d0fc97a94414929653af0fda73e4
elacevedo <76762112+elacevedo@users.noreply.github.com>
2022-05-08 18:59:43 -0500

    update with third requirement

--- statistic.py
+++ statistic.py
@@ -6,10 +6,11 @@ class Statistic:
    def __init__(self, text):
       self.text = text        #the original text string
       self.split_text = text  #the text that will be split into individual words
-      self.cmd = ''          #the cmd specified by the user to be worked on the text
+      self.cmd = ''           #the cmd specified by the user to be worked on the text
       self.word_list = []     #list of unique words in the text
       self.new_word = ''      #string that will replace the old string
       self.old_word = ''      #string to be replaced by the new string
+      self.keyword = ''       #string to search for within text
 
    #sets the strings used in replaceWords
    #oldword: old string that will be replaced the new string
@@ -24,47 +25,56 @@ class Statistic:
    #returns nothing
    def setCommand(self, command):
       self.cmd = command
+      
+   def setKeyword(self, word):
+      self.keyword = word
 
    #removes puncuation from given text string
    #returns nothing
    def removePuncuation(self):
       punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-      for ele in self.split_text:
+      for ele in self.text:
          if ele in punc:
-            self.split_text = self.split_text.replace(ele, ' ')
+            self.text = self.text.replace(ele, ' ')
 
    #removes any empty elements that occur when the text string is split
    #returns nothing
-   def removeEmpty(self):
-      while('' in self.split_text):
-         self.split_text.remove('')
+   def removeEmpty(self, text_list):
+      while('' in text_list):
+         text_list.remove('')
 
    #separates the original text string into a list that contains every word
    #returns nothing
    def separator(self):
       self.removePuncuation()
       self.split_text = re.split('\n|\t| ', self.text)
-            self.removeEmpty()
+      self.removeEmpty(self.split_text)
+      
+   def separateLines(self):
+      self.split_lines = re.split('\n', self.text)
+      self.removeEmpty(self.split_lines)
 
    #puts every unique string into a list
    #returns nothing
    def stringList(self):
+      self.separator()
       for i in self.split_text:
          if i not in self.word_list:
             self.word_list.append(i)
 
    def statistic(self):
       if self.cmd == 'frequency':
-         self.separator()
-         self.stringList()
          result = self.calcFrequency()
       elif self.cmd == 'replace':
          result = self.replaceWords()
+      elif self.cmd == 'grepline':
+         result = self.grepline()
       return result
 
    #counts the number of times a word appears in the given text
    #returns list with word count
    def calcFrequency(self):
+      self.stringList()
       word_freq = []
       for i in range(0, len(self.word_list)):
          word_freq.append(self.split_text.count(self.word_list[i]))
@@ -75,6 +85,14 @@ class Statistic:
    def replaceWords(self):
       new_text = self.text.replace(self.old_word, self.new_word)
       return new_text
+   
+   def grepline(self):
+      self.separateLines()
+      keywordLines = []
+      for line in self.split_lines:
+         if re.search(r"\b%s\b" % self.keyword, line) != None:
+            keywordLines.append(line)
+      return keywordLines
 
    def getWordList(self):
       return self.word_list
@@ -107,3 +125,9 @@ class TestStatistic(unittest.TestCase):
       self.command.setReplacementWords('Old', 'New')
       result = self.command.getStatistic()
       self.assertEqual(result, "New word\nNew word\nNew world\nNew world\n")
+      
+   def test_grepline(self):
+      self.command.setCommand('grepline')
+      self.command.setKeyword('New')
+      result = self.command.getStatistic()
+      self.assertEqual(result, ['New word', 'New world'])
